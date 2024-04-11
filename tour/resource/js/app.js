b25d02e2652b9ac0404bdb48b0ae12494910ba71
vanthanh140903 <thanhnvpd11342@gmail.com>
2024-04-11 09:57:56 +0700

    test1

---
+++ tour/resource/js/app.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+  const menuBtn = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+
+  // click --> 3 gạch --> add class vào.
+  menuBtn && menuBtn.addEventListener("click", inMenu);
+  function inMenu(event) {
+    menu && menu.classList.add("is-show");
+  }
+
+  // click -- ngoài menu --> remove class
+  document.addEventListener("click", outInMenu);
+  function outInMenu(event) {
+    if (event.target.matches(".menu-toggle") || event.target.matches(".menu"))
+      return;
+    menu && menu.classList.remove("is-show");
+  }
+});
+// load //
+$(window).on("load", function (event) {
+  $("body").removeClass("preloading");
+  $(".load").delay(1000).fadeOut("fast");
+});
